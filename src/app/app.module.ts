06a3847bada7a6cd9fbc34bd2363b5471454b76e
Uroš Vuković <57104944+UrosVukovic997@users.noreply.github.com>
2020-05-01 19:01:40 +0200

    -Otvaranje konekcije ka ws-u Korisceni materijal: https://tutorialedge.net/typescript/angular/angular-websockets-tutorial/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
+import { LogInComponent } from './log-in/log-in.component';
+import {AppRoutingModule} from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    RegistrationComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
